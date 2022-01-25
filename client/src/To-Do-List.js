38e53d08a30d1ccde7d6b62b95a5b7c9769bd577
gyulong1 <48115998+gyulong1@users.noreply.github.com>
2022-01-25 22:07:01 +0800

    Front end

---
+++ client/src/To-Do-List.js
@@ -0,0 +1,152 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
+
+let endpoint = "http://localhost:8080";
+
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: "",
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    this.getTask();
+  }
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log(this.state);
+
+  };
+
+  onSubmit = () => {
+    let { task } = this.state;
+    if (task) {
+      axios
+        .post(
+          endpoint + "/api/task", "task=".concat(task),
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          }
+        )
+        .then(res => {
+          this.getTask();
+          this.setState({
+            task: ""
+          });
+          console.log(res);
+        });
+    }
+  };
+
+  getTask = () => {
+    axios.get(endpoint + "/api/task").then(res => {
+      console.log(res);
+      if (res.data) {
+        console.log(res.data)
+
+        this.setState({
+          items: res.data.map(item => {
+            let color = "yellow";
+
+            if (item.Done) {
+              color = "green";
+            }
+            return (
+              <Card key={item.Id} color={color} fluid>
+                <Card.Content>
+                  <Card.Header textAlign="left">
+                    <div style={{ wordWrap: "break-word" }}>{item.Desc}</div>
+                  </Card.Header>
+
+                  <Card.Meta textAlign="right">
+                    <Icon
+                      name="check circle"
+                      color="green"
+                      onClick={() => this.updateTask(item.Id, item.Desc, true)}
+                    />
+                    <span style={{ paddingRight: 10 }}>Done</span>
+                    <Icon
+                      name="delete"
+                      color="red"
+                      onClick={() => this.deleteTask(item.Id)}
+                    />
+                    <span style={{ paddingRight: 10 }}>Delete</span>
+                  </Card.Meta>
+                </Card.Content>
+              </Card>
+            );
+          })
+        });
+      } else {
+        this.setState({
+          items: []
+        });
+      }
+    });
+  };
+
+  updateTask = (id) => {
+    axios
+      .put(endpoint + "/api/toggleTask/" + id, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      .then(res => {
+        console.log(res);
+        this.getTask();
+      });
+  };
+
+  deleteTask = id => {
+    axios
+      .delete(endpoint + "/api/deleteTask/" + id, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      .then(res => {
+        console.log(res);
+        this.getTask();
+      });
+  };
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <Header className="header" as="h2">
+            To Do List
+          </Header>
+        </div>
+        <div className="row">
+          <Form onSubmit={this.onSubmit}>
+            <Input
+              type="text"
+              name="task"
+              onChange={this.onChange}
+              value={this.state.task}
+              fluid
+              placeholder="Create Task"
+            />
+            {/* <Button >Create Task</Button> */}
+          </Form>
+        </div>
+        <div className="row">
+          <Card.Group>{this.state.items}</Card.Group>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ToDoList;
