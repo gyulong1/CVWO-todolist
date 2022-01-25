9e2aeed426bb141b299cba451ff2877ad9a916ca
gyulong1 <48115998+gyulong1@users.noreply.github.com>
2022-01-26 03:16:52 +0800

    added comments

--- server/main.go
+++ server/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    _ "github.com/gorilla/mux"
+	"fmt"
+	_ "github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"log"
+	"net/http"
 )
 
 func main() {
-    r := router()
-    fmt.Println("Starting server on the port 8080...")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	// starts the router
+	r := Router()
+	fmt.Println("Starting server on the port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- server/router.go
+++ server/router.go
@@ -4,16 +4,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Router is exported and used in main.go
-func router() *mux.Router {
-
-	Init()
+func Router() *mux.Router {
+	// initializes connectinon to pg database
+	db_Init()
 	router := mux.NewRouter()
+
 	//router.HandleFunc("/api/login", middleware.Login).Methods("POST", "OPTIONS")
 	//router.HandleFunc("/api/register", middleware.Register).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/task", GetTask).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/task", CreateTask).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/toggleTask/{id}", ToggleTask).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteTask/{id}", DeleteTask).Methods("DELETE", "OPTIONS")
+
 	return router
 }
